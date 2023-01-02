4251a2d69d5c63ed21d67a148450598a64c09fa0
Nicole Sweeden <sweeden.nicole@gmail.com>
2023-01-02 11:31:28 -0700

    finished exercise 2 for java

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int myFavoriteNumber = 11;
+        System.out.println(myFavoriteNumber);
+        String myString = "I am a stringy string.";
+        System.out.println(myString);
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+//        int x = 10;
+//        int y = 2;
+////        x /= y;
+//        y -= x;
+//        System.out.println(y);
+        long max = 3,147,483,647;
+        System.out.println(max);
+
+    }
+}
+
