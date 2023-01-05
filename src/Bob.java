22669d39906051d99a4878a18beeb0a2d0d92dd1
Nicole Sweeden <sweeden.nicole@gmail.com>
2023-01-05 14:30:38 -0700

    working on the Bob exercise

--- src/Bob.java
+++ src/Bob.java
@@ -19,10 +19,7 @@ public class Bob {
 //            }
 //            }
 
-        System.out.println("Say something to Bob:");
-        Scanner scan = new Scanner(System.in);
 
-        String userInput = scan.nextLine();
 
 //        if (userInput.endsWith("?")){
 //            System.out.println("Sure");
@@ -35,34 +32,47 @@ public class Bob {
 //        }
 
 
-        while (true) {
-            System.out.println("Say something to Bob:");
-            if (userInput.endsWith("?")){
-                System.out.println("Sure");
-                System.out.println("Say something to Bob again:");
-            } else if (userInput.endsWith("!")){
-                System.out.println("Whoa, chill out!");
-                System.out.println("Say something to Bob again:");
-            } else if (userInput.endsWith("")){
-                System.out.println("Fine. Be that way!");
-                System.out.println("Say something to Bob again:");
-        } else {
-                System.out.println("Whatever");
-            }
-        }
+//        while (true) {
+//            System.out.println("Say something to Bob:");
+//            if (userInput.endsWith("?")){
+//                System.out.println("Sure");
+//                System.out.println("Say something to Bob again:");
+//            } else if (userInput.endsWith("!")){
+//                System.out.println("Whoa, chill out!");
+//                System.out.println("Say something to Bob again:");
+//            } else if (userInput.endsWith("")){
+//                System.out.println("Fine. Be that way!");
+//                System.out.println("Say something to Bob again:");
+//        } else {
+//                System.out.println("Whatever");
+//            }
+//        }
+
+        Scanner scan = new Scanner(System.in);
+        boolean cont;
 
 //        This code worked but only once
-//        do {
-//            if (userInput.endsWith("?")) {
-//                System.out.println("Sure");
-//            } else if (userInput.endsWith("!")) {
-//                System.out.println("Whoa, chill out!");
-//            } else if (userInput.endsWith(" ")) {
-//                System.out.println("Fine. Be that Way!");
-//            } else {
-//                System.out.println("Whatever.");
-//            }
-//        } while (userInput == "");
+        do {
+            System.out.println("Say something to Bob:");
+            String userInput = scan.nextLine();
+
+            if (userInput.endsWith("?")) {
+                System.out.println("Sure");
+            } else if (userInput.endsWith("!")) {
+                System.out.println("Whoa, chill out!");
+            } else if (userInput.endsWith(" ")) {
+                System.out.println("Fine. Be that Way!");
+            } else {
+                System.out.println("Whatever.");
+            }
+            System.out.println("Would you like to keep talking to Bob? {Y/N}");
+            String contInput = scan.nextLine();
+            if (contInput.equalsIgnoreCase("Y")) {
+                cont = true;
+            } else {
+                cont = false;
+            } scan.nextLine();
+        } while (cont);
 
 
     }
---
+++ src/MethodsExercises.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class MethodsExercises {
+//    public static int sum(int a, int b) {
+//        System.out.println("Enter two numbers for addition: ");
+//        Scanner userInput = new Scanner(System.in);
+//        int a = userInput.nextInt();
+//        int b = userInput.nextInt();
+//        userInput.close();
+//        System.out.println("The sum of a and b is = " + Integer.sum(int a, int b));
+//    }
+
+
+
+
+
+
+
+
+
+
+
+
+//    public static void main() {
+//    }
+
+
+}
+
