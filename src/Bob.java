2dac3add66b0f9aa0897704bf6c38dad9bec10d9
Nicole Sweeden <sweeden.nicole@gmail.com>
2023-01-06 08:32:28 -0700

    setup an exercise

--- src/Bob.java
+++ src/Bob.java
@@ -2,56 +2,10 @@ import java.util.Scanner;
 
 public class Bob {
     public static void main(String[] args) {
-//
-//        String firstStatement = "We don't need no education.\n" +
-//            "We don't need no thought control.";
-//            System.out.print(firstStatement);
-
-//Scanner scanner = new Scanner(System.in);
-//
-//        for (int i=1; i<=100; i++) {
-//            if (i%3 == 0 && i%5 == 0) {
-//                System.out.println("FizzBuzz");
-//            } else if (i%3 == 0) {
-//                System.out.println("Fizz");
-//            } else {
-//                System.out.println("Buzz");
-//            }
-//            }
-
-
-
-//        if (userInput.endsWith("?")){
-//            System.out.println("Sure");
-//        } else if (userInput.endsWith("!")) {
-//            System.out.println("Whoa, chill out!");
-//        } else if (userInput.endsWith("")) {
-//            System.out.println("Fine. Be that Way!");
-//        } else {
-//            System.out.println("Whatever.");
-//        }
-
-
-//        while (true) {
-//            System.out.println("Say something to Bob:");
-//            if (userInput.endsWith("?")){
-//                System.out.println("Sure");
-//                System.out.println("Say something to Bob again:");
-//            } else if (userInput.endsWith("!")){
-//                System.out.println("Whoa, chill out!");
-//                System.out.println("Say something to Bob again:");
-//            } else if (userInput.endsWith("")){
-//                System.out.println("Fine. Be that way!");
-//                System.out.println("Say something to Bob again:");
-//        } else {
-//                System.out.println("Whatever");
-//            }
-//        }
 
         Scanner scan = new Scanner(System.in);
         boolean cont;
 
-//        This code worked but only once
         do {
             System.out.println("Say something to Bob:");
             String userInput = scan.nextLine();
@@ -65,15 +19,19 @@ public class Bob {
             } else {
                 System.out.println("Whatever.");
             }
+
             System.out.println("Would you like to keep talking to Bob? {Y/N}");
             String contInput = scan.nextLine();
+
             if (contInput.equalsIgnoreCase("Y")) {
                 cont = true;
             } else {
                 cont = false;
-            } scan.nextLine();
-        } while (cont);
+            }
 
+            scan.nextLine();
+
+        } while (cont);
 
     }
 }
--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,28 +1,41 @@
 import java.util.Scanner;
 
 public class MethodsExercises {
-//    public static int sum(int a, int b) {
-//        System.out.println("Enter two numbers for addition: ");
-//        Scanner userInput = new Scanner(System.in);
-//        int a = userInput.nextInt();
-//        int b = userInput.nextInt();
-//        userInput.close();
-//        System.out.println("The sum of a and b is = " + Integer.sum(int a, int b));
-//    }
+
+//    1a through 1d
+    public static int sum (int a, int b) {
+        return a + b;
+    }
+
+    public static int difference (int a, int b) {
+        return a - b;
+    }
+
+    public static int multiplication (int a, int b) {
+        return a * b;
+    }
+
+    public static int division (int a, int b) {
+        return a / b;
+    }
+
+    public static int modulus (int a, int b) {
+        return a % b;
+    }
+
+//    tester method
+    public static void main(String[] args) {
+        System.out.println(modulus(18, 6));
+    }
 
 
+//    2
+    public static int getInteger(int min, int max) {
+    Scanner myObj = new Scanner(System.in);  // Creating a Scanner object
+    System.out.println("Enter a random number:");
+    String userName = myObj.nextLine();  // Read user input
 
-
-
-
-
-
-
-
-
-
-//    public static void main() {
-//    }
+    }
 
 
 }
