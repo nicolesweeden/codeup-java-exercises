28fed57897f30044500c929895daebb196df71ce
Nicole Sweeden <sweeden.nicole@gmail.com>
2023-01-10 14:55:51 -0700

    finished ConsoleExercises

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -28,11 +28,21 @@ public class ConsoleExercises {
 //        new Scanner(System.in);
 //
 //        System.out.print("Enter a sentence: ");
-//        String userInputSentence = scanner.next();
+//        String userInputSentence = scanner.nextLine();
 //
 //        System.out.println("You entered: " + userInputSentence + ".");
 
-//        Not done with this exercise!
+//Calculate the perimeter and area of Codeup's classrooms.
+//#1
+    new Scanner(System.in);
 
-}
+        System.out.print("Enter the length in feet of a room to calculate it's dimensions: ");
+        String userInputLength = scanner.nextLine();
+        System.out.print("Enter the width in feet of a room to calculate it's dimensions: ");
+        String userInputWidth = scanner.nextLine();
+        int l = Integer.parseInt(userInputLength);
+        int w = Integer.parseInt(userInputWidth);
+        System.out.println("You entered " + l + " feet for the length " + "and " + w + " feet for the width. The perimeter of your room is " + (2*l + 2*w) + " feet.");
+        System.out.println("The area of your room is " + (l*w) + " ft^2.");
+    }
 }
---
+++ src/util.java
@@ -0,0 +1,14 @@
+//package util;
+//
+//import java.util.Scanner;
+//
+//public class Input {
+//
+//    public static void main(String[] args) {
+//    private Scanner scanner;
+//    this.scanner = new Scanner(System.in);
+//
+//
+
+//    }//end main
+//}//end Input
