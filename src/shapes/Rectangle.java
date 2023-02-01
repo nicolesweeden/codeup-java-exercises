85795bcd9575ac6ca738166b8901d807c59912b7
Nicole Sweeden <sweeden.nicole@gmail.com>
2023-02-01 14:07:26 -0700

    saving just in case

---
+++ src/shapes/Rectangle.java
@@ -0,0 +1,36 @@
+////package shapes;
+////
+//////public class Rectangle extends Quadrilateral implements Measurable{
+//////
+//////    public Rectangle(double widthNum, double lengthNum) {
+//////        super(widthNum, lengthNum);
+//////    }
+//////
+//////    @Override
+//////    public double setWidth() {
+//////        return width;
+//////    }
+//////
+//////    @Override
+//////    public double setLength() {
+//////        return length;
+//////    }
+//////}
+////==============================================CODE FOR FIRST ASSIGNMENT===============================================
+//public class Rectangle {
+//    protected int length;
+//    protected int width;
+//    protected int side;
+////        Rectangle should define a constructor that accepts two numbers for length and width, and sets those properties.
+//public Rectangle(int widthNum, int lengthNum) {
+//    this.width = widthNum;
+//    this.length = lengthNum;
+//}
+//public int getArea() {
+//    return width * length;
+//}
+//public int getPerimeter() {
+//    return (2 * length) + (2 * width);
+////}
+////
+////}
---
+++ src/shapes/ShapesTest.java
@@ -0,0 +1,29 @@
+//package shapes;
+//
+//public class ShapesTest {
+//
+//
+//    public static void main(String[] args) {
+//
+//        Measurable myShape = new Square(5);
+//        Measurable myShape2 = new Rectangle(4,5 );
+//        System.out.println(myShape.getArea());
+//        System.out.println(myShape.getPerimeter());
+//        System.out.println(myShape2.getArea());
+//        System.out.println(myShape2.getPerimeter());
+////==============================================CODE FOR FIRST ASSIGNMENT===============================================
+//
+////        create a variable of the type Rectangle named box1 and assign it a new instance of the Rectangle class with a width of 4 and a length of 5
+////        verify that the getPerimeter and getArea methods return 18 and 20, respectively.
+////        create a variable of the type Rectangle named box2 and assign it a new instance of the Square class that has a side value of 5.
+////        verify that the getPerimeter and getArea methods return 20 and 25, respectively
+//
+//        Rectangle box1 = new Rectangle(4,5);
+//        Rectangle box2 = new Square(5);
+//        System.out.println(box1.getArea());
+//        System.out.println(box1.getPerimeter());
+//        System.out.println(box2.getArea());
+//        System.out.println(box2.getPerimeter());
+//
+//    }
+//}
